6a507d591a0c72cb06b63486b4d227f0ef32e3fc
nimedev <nicolasmejiaco@gmail.com>
2016-05-01 18:30:54 -0500

    firts release

---
+++ README.md
@@ -0,0 +1,46 @@
+# eslint-config-nimedev
+[![npm][npm-image]][npm-url]
+
+[npm-image]: https://img.shields.io/npm/v/eslint-config-nimedev.svg
+[npm-url]: https://npmjs.org/package/eslint-config-nimedev
+
+> An ESLint [Shareable Config](http://eslint.org/docs/developer-guide/shareable-configs) for nimedev JavaScript rules
+
+## Installation
+
+```console
+$ npm install eslint-config-nimedev
+```
+
+## Usage
+
+Set your eslint config to:
+
+```json
+{
+  "extends": "nimedev"
+}
+```
+
+*Note: I omitted the `eslint-config-` prefix since it is automatically assumed by ESLint.*
+
+### Extending the config
+
+Simply add a `"rules"` key to your config and add your overrides there.
+
+For example, to change the `indentation` to tabs and turn off the `no-console` rule:
+
+
+```json
+{
+  "extends": "nimedev",
+  "rules": {
+    "indentation": "tab",
+    "no-console": 0
+  }
+}
+```
+
+## [Changelog](CHANGELOG.md)
+
+## [License](LICENSE)
---
+++ index.js
@@ -0,0 +1,54 @@
+module.exports = {
+  "extends": "eslint:recommended",
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "max-len": [
+      "error",
+      80,
+      {
+        "ignorePattern": "`",
+        "ignoreUrls": true
+      }
+    ],
+    "no-trailing-spaces": [
+      "error"
+    ],
+    "no-whitespace-before-property": "error",
+    "quotes": [
+      "error",
+      "single",
+      {
+        "allowTemplateLiterals": true
+      }
+    ],
+    "require-jsdoc": [
+      "error",
+      {
+        "require": {
+          "FunctionDeclaration": true,
+          "MethodDefinition": true,
+          "ClassDeclaration": true
+        }
+      }
+    ],
+    "semi": [
+      2,
+      "never"
+    ],
+    "spaced-comment": [
+      "error",
+      "always"
+    ]
+  }
+}
